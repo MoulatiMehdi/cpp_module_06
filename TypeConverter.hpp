3fe1450bb68256576cd4ad87bfffd8f8619d9fc7
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-12-13 16:13:28 +0100

    push tests files
    add Converter class
    
    change name

---
+++ TypeConverter.hpp
@@ -0,0 +1,39 @@
+#ifndef CONVERTER_HPP
+#define CONVERTER_HPP
+
+#include <string>
+
+class TypeConverter
+{
+  private:
+    TypeConverter();
+    TypeConverter(const std::string &str);
+    TypeConverter(const TypeConverter &other);
+    ~TypeConverter();
+    TypeConverter &operator=(const TypeConverter &other);
+
+  public:
+    enum Type
+
+    {
+        Char,
+        Int,
+        Float,
+        Double,
+        Unknown
+    };
+
+    typedef struct
+    {
+        bool (*func)(const std::string &str);
+        Type type;
+    } TypeAction;
+
+    static bool isChar(const std::string &str);
+    static bool isDouble(const std::string &str);
+    static bool isInt(const std::string &str);
+    static bool isFloat(const std::string &str);
+    static Type getLiteralType(const std::string &str);
+};
+
+#endif
---
+++ tests/Converter.test.cpp
@@ -0,0 +1,51 @@
+#include "Converter.hpp"
+#include <iomanip>
+#include <ios>
+#include <iostream>
+#include <string>
+
+typedef struct
+{
+    std::string name;
+    bool (*func)(const std::string &);
+} action;
+
+int main()
+
+{
+    std::string arr[] = {
+        ".",    ".5",   ".1.5", ".15.",  "1",     "1.",    "1.5",
+        "1..5", "1.5.", "f",    ".f",    ".5f",   ".1.5f", ".15.f",
+        "1f",   "1.f",  "1.5f", "1..5f", "1.5.f",
+    };
+    int    size       = sizeof(arr) / sizeof(arr[0]);
+    action actions[4] = {
+        {   "int",    Converter::isInt},
+        {  "char",   Converter::isChar},
+        { "float",  Converter::isFloat},
+        {"double", Converter::isDouble},
+    };
+
+    std::cout << std::string(5 * 12 + 5, '-') << std::endl;
+    std::cout << "|" << std::setw(10) << std::left << "number";
+    for (int j = 0; j < 4; j++)
+    {
+        std::cout << " | " << std::setw(10) << std::boolalpha
+                  << actions[j].name;
+    }
+    std::cout << " |" << std::endl;
+    std::cout << std::string(5 * 12 + 5, '-') << std::endl;
+    for (int i = 0; i < size; i++)
+    {
+        std::cout << "|" << std::setw(10) << std::left << arr[i];
+        for (int j = 0; j < 4; j++)
+        {
+            std::cout << " | " << std::setw(10) << std::boolalpha
+                      << actions[j].func(arr[i]);
+        }
+        std::cout << " |" << std::endl;
+        std::cout << std::string(5 * 12 + 5, '-') << std::endl;
+    }
+
+    return 0;
+}
