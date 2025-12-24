d453722cb965b1d6a15c4e6c5f7b47350a590d9e
MoulatiMehdi <moulati.mehdi@gmail.com>
2025-12-24 16:17:44 +0100

    update the test file of the DoubleType

--- tests/DoubleType.test.cpp
+++ tests/DoubleType.test.cpp
@@ -1,11 +1,11 @@
-#include "IntType.hpp"
+#include "DoubleType.hpp"
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
         return -1;
 
-    IntType a(argv[1]);
+    DoubleType a(argv[1]);
 
     a.convert();
 }
